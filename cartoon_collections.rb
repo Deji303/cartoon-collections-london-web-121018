5121559360541175fb330bbd2f3636bd9a2e920d
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-11-20 16:43:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,6 +22,6 @@ end
 def find_the_cheese (food)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  food.find {|x| x == cheese_types.each {|y| y} }
+  food.find{|x| x == cheese_types.each{|y| y} }
 end
 
